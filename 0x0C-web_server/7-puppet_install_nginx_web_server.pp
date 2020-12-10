ee1eb3b96cd16c76d61befa0793e5bb82f8d9e8e
oimoralest <oimoralest@gmail.com>
2020-12-09 20:25:37 -0500

    Modified

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,7 +11,7 @@ file_line {'append permanent redirection':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => '   rewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 service {'nginx':
   ensure  => running,
