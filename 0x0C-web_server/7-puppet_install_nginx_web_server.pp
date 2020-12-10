9b2708c42fd94fffaaf82701bfa0a43089d8596c
oimoralest <oimoralest@gmail.com>
2020-12-09 20:21:21 -0500

    Deleted path directive in file

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,15 +4,14 @@ include stdlib
 package {'nginx':
   ensure => 'installed',
 }
-file {'create index.html':
-  path    => '/var/www/html/index.html',
+file {'/var/www/html/index.html':
   content => 'Holberton School',
 }
 file_line {'append permanent redirection':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'server_name _;',
-  line   => '       rewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => '   rewrite ^/redirect_me/$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 service {'nginx':
   ensure  => running,
