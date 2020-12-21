bdb64efe2a7a664df308055cfbf0d7f1d6c80051
oimoralest <oimoralest@gmail.com>
2020-12-21 16:03:12 -0500

    Modified

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,16 +14,6 @@ file_line { 'append instruction':
   after  => 'server_name _;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
-file_line { 'append instruction':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'error_page 404  /custom_404.html;',
-}
-file { 'custom_404.html':
-  content => "Ceci n'est pas une page"
-  path    => '/var/www/html/custom_404.html'
-}
 file_line { 'add_header':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
