2a859c71477fb71e62207d3711f8b36ee259bb20
oimoralest <oimoralest@gmail.com>
2020-12-21 16:40:31 -0500

    Fixed providder by provider

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,8 +2,8 @@
 
 exec { 'Nginx header':
   command   => 'sudo apt-get update;
-sudo apt-get -y install nginx;
-sudo sed -i "/server_name _;/ a\ \ \ \ \ \ \ \ add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-sudo service nginx restart',
-  providder => 'shell',
+  sudo apt-get -y install nginx;
+  sudo sed -i "/server_name _;/ a\ \ \ \ \ \ \ \ add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
+  provider => 'shell',
 }
