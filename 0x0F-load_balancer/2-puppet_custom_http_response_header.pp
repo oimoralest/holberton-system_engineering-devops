b177e256f2320d83902ce2db78f738ab6e148939
oimoralest <oimoralest@gmail.com>
2020-12-21 16:34:56 -0500

    Modified

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -14,11 +14,9 @@ file_line { 'append instruction':
   after  => 'server_name _;',
   line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
-file_line { 'add_header':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'add_header X-Served-By $hostname;',
+exec { 'append http header':
+  command => 'sed -i "/server_name _;/ a\ \ \ \ \ \ \ \ add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/defaul',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }
 service {'nginx':
   ensure  => running,
