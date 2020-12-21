36d7d7c86480169ee56756ff65b50fd252d7656a
oimoralest <oimoralest@gmail.com>
2020-12-21 16:38:58 -0500

    Created again

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,9 @@
 # This manifest configures a web server with nginx
 
-package { 'nginx':
-  ensure => installed,
-}
-
-file { 'index.html':
-  content => 'Holberton School',
-  path    => '/var/www/html/index.html'
-}
-file_line { 'append instruction':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
-}
-exec { 'append http header':
-  command => 'sed -i "/server_name _;/ a\ \ \ \ \ \ \ \ add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/defaul',
-  path    => '/usr/bin:/usr/sbin:/bin',
-}
-service {'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'Nginx header':
+  command   => 'sudo apt-get update;
+sudo apt-get -y install nginx;
+sudo sed -i "/server_name _;/ a\ \ \ \ \ \ \ \ add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+sudo service nginx restart',
+  providder => 'shell',
 }
