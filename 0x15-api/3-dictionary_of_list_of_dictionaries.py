97c2a939d5178016fa1edbae27c54a6c80591c1a
oimoralest <oimoralest@gmail.com>
2021-02-01 15:46:46 -0500

    Added Python script to export data in the JSON format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This script records all tasks from all employees in a json file
+"""
+import json
+import requests
+
+
+if __name__ == '__main__':
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todoDictionary = {}
+    for user in users:
+        tasks = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId=' +
+            str(user.get('id'))).json()
+        tasks = [
+            {'username': user.get('name'), 'task': task.get('title'),
+             'completed': task.get('completed')} for task in tasks]
+        todoDictionary.update({user.get('id'): tasks})
+    with open('todo_all_employees.json', mode='w') as employeeFile:
+        json.dump(todoDictionary, employeeFile, indent=4)
