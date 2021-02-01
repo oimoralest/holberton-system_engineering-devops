5df927ebc7aa58e4100f848d2f2bf8c6abaeeb7c
oimoralest <oimoralest@gmail.com>
2021-02-01 15:42:58 -0500

    Organized importation by alphabetical order

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,9 +2,9 @@
 """This script, using a REST API, for a given employee ID, returns information
 about his/her TODO list progress
 """
+import csv
 import requests
 from sys import argv
-import csv
 
 
 if __name__ == '__main__':
