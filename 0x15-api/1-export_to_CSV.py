b722ca5bbbc36b8d0b3724a31b29c24ae3d39e4e
oimoralest <oimoralest@gmail.com>
2021-02-01 15:40:15 -0500

    Added script

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This script, using a REST API, for a given employee ID, returns information
+about his/her TODO list progress
+"""
+import requests
+from sys import argv
+import csv
+
+
+if __name__ == '__main__':
+    userInfo = requests.get(
+        'https://jsonplaceholder.typicode.com/users/' + argv[1]).json()
+    tasks = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=' + argv[1]).json()
+    with open('{}.csv'.format(argv[1]), mode='w') as employeeFile:
+        writer = csv.writer(
+            employeeFile, delimiter=',', quotechar='"',
+            quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow(
+                [userInfo.get('id'), userInfo.get('name'),
+                 task.get('completed'), task.get('title')])
