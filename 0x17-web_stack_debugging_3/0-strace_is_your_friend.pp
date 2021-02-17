2f16484ae1e6f31fc6950dd5dbee6efc9b232bce
oimoralest <oimoralest@gmail.com>
2021-02-17 18:04:35 -0500

    Added Puppet manifest

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Fix an apache2 server
+exec { 'fix':
+    command => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+    path    => ['/usr/bin', '/usr/sbin', '/usr/local/bin', '/usr/local/sbin'],
+}
