18edc75509223e951c91b8f0e979cf8a6c3729b9
oimoralest <oimoralest@gmail.com>
2021-02-25 17:42:16 -0500

    Fixed command to restart nginx service

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,6 @@
 # This manifest fix the read limit for nginx
 exec {'sed':
-  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/" /etc/default/nginx',
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/" /etc/default/nginx;
+              service nginx restart',
   path    => '/usr/bin/'
-}
-service {'nginx':
-  ensure  => true,
-  restart => 'service nginx restart'
 }
