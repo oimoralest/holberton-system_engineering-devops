ccb95f8d7d7036dd5624209f8f00cfd08527bd58
oimoralest <oimoralest@gmail.com>
2021-02-25 09:05:30 -0500

    Modified path for command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # This manifest fix the read limit for nginx
 exec {'sed':
   command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4000\"/" /etc/default/nginx',
-  path    => '/bin/'
+  path    => '/usr/bin/'
 }
 service {'nginx':
   ensure  => true,
